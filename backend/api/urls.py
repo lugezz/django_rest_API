b5eefca2c779a8bf2bd8ffd025028a64fa41c9bc
Eugenio <lugezz@gmail.com>
2023-05-08 09:14:33 -0300

    Some updates

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,9 +17,12 @@ urlpatterns = [
     path('search/', include('search.urls')),
 
     path('auth/', obtain_auth_token),
+
+    # JWT
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('raw-home/', raw_home, name='raw_home'),
     path('raw-product/', raw_product, name='raw_product'),
     path('mdt-product/', mdt_product, name='mdt_product'),
--- backend/products/views.py
+++ backend/products/views.py
@@ -58,8 +58,8 @@ class ProductListCreateAPIView(UserQuerysetMixin,
 
     def perform_create(self, serializer):  # Frena antes de grabar
         title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or None
-        if content is None:
+        content = serializer.validated_data.get('content')
+        if not content:
             content = title
 
         serializer.save(user=self.request.user, content=content)  # Listo, lo graba
